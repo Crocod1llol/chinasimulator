18dda95067fe3511b6e59ca6744d8805c4c0e21d
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-03 18:37:29 +0300

    added warning flags and included the other file

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-g++ main.cpp -o puya.exe -I./lib -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/home.cpp -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
