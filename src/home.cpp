f79180ace4400c41b6597745999669c0f209470e
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-04 23:08:18 +0300

    fixed bug: it now points to the actual object instead of a copy of it
    i remade the home_int_parts vector to store pointers of parts, so the parts themselfs get modified instead of the copy in the vector
    
    and with that, the "GUI" finally renders

--- src/home.cpp
+++ src/home.cpp
@@ -6,23 +6,33 @@ extern "C" {
 
 #include "headers/general_parts.hpp"
 
-//vectors containing all Parts
-std::vector<interact_part> int_parts;
-std::vector<Part> parts;
 
-std::vector<interact_part> doors;
+//vectors containing all Parts
+//store pointers pf the interact parts so we can actually so the copies in the vector affect the real obj
+std::vector<interact_part*> home_int_parts;
+
+std::vector<Part> home_parts;
+
+std::vector<interact_part> home_doors;
 
 //doors will be their own object since they go to different rooms
 interact_part outside_door_from_home;
+
+//separate interact items
 interact_part tester1;
+
+interact_part tester2;
 
 void init_home() {
 
     //initiate objects when the window is open
     outside_door_from_home = {350, 0, 90, 150, LoadTexture("resources/img/door_90x150.png")};
     tester1 = {20, 300, 100, 75, LoadTexture("resources/img/test_100x75.png")};
+    tester2 = {600, 220, 100, 75, LoadTexture("resources/img/test_100x75.png")};
 
     //push to vectors
-    int_parts.push_back(tester1);
-    doors.push_back(outside_door_from_home);
+    home_int_parts.push_back(&tester1);
+    home_int_parts.push_back(&tester2);
+
+    home_doors.push_back(outside_door_from_home);
 }
