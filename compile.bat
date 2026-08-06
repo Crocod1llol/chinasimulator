8d37f0dfa973e0e206b0fac3fb3da6804fa7ddb2
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-06 22:31:04 +0300

    added file to compile

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/home.cpp -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/home.cpp ./src/outside_home.cpp -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
