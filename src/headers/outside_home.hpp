b90c0411b13a9db27ff546058a0479cccfd83541
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-06 22:30:18 +0300

    added header to use

---
+++ src/headers/outside_home.hpp
@@ -0,0 +1,26 @@
+#ifndef OUTSIDE_HOME_HPP
+#define OUTSIDE_HOME_HPP
+
+//extern structs
+struct Texture;
+struct Part;
+struct interact_part;
+
+//created structs
+//parts
+extern Part maybe_flowers;
+
+//exits
+extern interact_part outside_door_to_home;
+
+//vectors
+extern std::vector <Part> outside_home_parts;
+extern std::vector <interact_part*> outside_exits;
+
+//funcs
+void init_outside_home();
+
+//unique texture
+extern Texture outside_home_grass;
+
+#endif
