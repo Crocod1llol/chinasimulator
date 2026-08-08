3e9b32fda447d5e984f0697385ad59be473179c7
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-08 23:00:02 +0300

    added new file to compile
    items.cpp

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/home.cpp ./src/outside_home.cpp ./src/player.cpp -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/player.cpp ./src/home.cpp ./src/outside_home.cpp ./src/items.cpp -L./lib -lraylib -lm -lpthread -ldl -lrt -lX11
