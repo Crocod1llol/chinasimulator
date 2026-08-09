b237460a35bb9b88ea86e9515236761f4cf84eff
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-09 09:53:59 +0300

    updated texture paths to the new one

--- src/outside_home.cpp
+++ src/outside_home.cpp
@@ -23,14 +23,14 @@ interact_part outside_door_to_home;
 
 void init_outside_home() {
     //the outside grass
-    outside_home_grass = LoadTexture("resources/img/outside_grass_home_2000x2000.png");
+    outside_home_grass = LoadTexture("resources/img/textures/outside_grass_home_2000x2000.png");
     //definition
 
     //interactable parts
-    outside_door_to_home = {400, 450, 90, 150, LoadTexture("resources/img/door_90x150.png")};
+    outside_door_to_home = {400, 450, 90, 150, LoadTexture("resources/img/textures/door_90x150.png")};
 
     //Parts
-    maybe_flowers = {75, 170, 150, 100, LoadTexture("resources/img/bad_flowers_150x100.png")};
+    maybe_flowers = {75, 170, 150, 100, LoadTexture("resources/img/textures/bad_flowers_150x100.png")};
 
 
     //inserting into vectors
