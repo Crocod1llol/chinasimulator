960b05cb5ce64651a323503a620a94c1953d59b5
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-09 20:52:28 +0300

    added new structs
    containers which help me make containers and item, which i think ill remake

--- src/headers/general_parts.hpp
+++ src/headers/general_parts.hpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <stddef.h>
+
 #ifndef GENERAL_PARTS_HPP
 #define GENERAL_PARTS_HPP
 
@@ -34,5 +37,55 @@ typedef struct interact_part {
 
 } interact_part;
 
+//struct to help me make parts made to store stuff
+typedef struct container {
+
+    float x;
+    float y;
+
+    float sizeX;
+    float sizeY;
+
+    Texture tex;
+
+    bool isInteracted = false;
+
+    const size_t max_items;
+
+    std::vector <size_t> container_item_ids;
+
+    //the hitbox is larger so the player can interact with it without colliding directly into it
+    Rectangle hitbox = {x, y, sizeX + 30, sizeY + 30};
+
+} container;
+
+//a struct to help me manage items
+typedef struct item {
+
+    float x;
+    float y;
+
+    //the item's id
+    //check misc for more info
+    size_t id;
+
+    //the status, whether its in a container or on the floor
+    //0 - in inventory
+    //1 - in container
+    //2 - on the floor
+    int status;
+
+    const char *name;
+
+    Texture2D tex;
+
+    //helpful for checking interactions
+    //default is false (0)
+    bool isInteracted = false;
+
+    //its hitbox
+    Rectangle hitbox = {x, y, 32, 32};
+
+} item;
 
 #endif
