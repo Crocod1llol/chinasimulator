3a0a86d5000985ac358f24b49649c3c118e1f228
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-09 20:50:12 +0300

    remade my decision and made item ids start at 1
    the 0 will be an empty slot

--- src/items.cpp
+++ src/items.cpp
@@ -2,11 +2,12 @@ extern "C" {
     #include "../lib/raylib.h"
 }
 
-//#include <vector>
+#include "headers/general_parts.hpp"
+#include <vector>
 
 //array that contains all possible item ids
 //check misc/all_item_ids.txt for all ids and respective object
-const int all_ids[] = {0};
+const int all_ids[] = {0, 1};
 
 
 //other resources
@@ -15,25 +16,37 @@ const int all_ids[] = {0};
 Texture2D test_object_0;
 
 //sound
-
 Sound troll;
 
+
+// a vector to keep track of the spawned items
+std::vector <item*> spawned_items;
+
+//item structs
+//check the general_parts header for more info
+item test_item;
 
 //load all item textures and init stuff
 void init_items() {
     //init textures
-    test_object_0 = LoadTexture("resources/img/textures/items/0_test_obj.png");
+    test_object_1 = LoadTexture("resources/img/textures/items/1_test_obj.png");
 
 
     //init sounds
     troll = LoadSound("resources/sfx/items/troll.ogg");
+
+    //init all structs
+    test_item = {100, 200, 1, 2, "test obj", test_object_0};
+
+    //insert into vectors
+    spawned_items.push_back(&test_item);
 }
 
 //ALL ITEM EXECUTION FUNCTIONS
 //format: *return type* *item name*_*id_func(*args*)
-//example: void test_obj_0_func()
+//example: void test_obj_1_func()
 
-void test_obj_0_func() {
+void test_obj_1_func() {
     
     //play a sound
     PlaySound(troll);
