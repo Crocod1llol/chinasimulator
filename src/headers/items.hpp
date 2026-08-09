2a5fc3031a8ceedf5caa86898cb5fa3c7d3342ed
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-09 20:51:17 +0300

    remade my decision and made item ids start at 1
    the 0 item id is blank

--- src/headers/items.hpp
+++ src/headers/items.hpp
@@ -1,5 +1,10 @@
+#include <vector>
+
 #ifndef ITEMS_HPP
 #define ITEMS_HPP
+
+//extern structs
+struct item;
 
 //array that contains all possible item ids
 //check misc/all_item_ids.txt for all ids and respective object
@@ -9,19 +14,25 @@ extern const int all_ids[];
 
 //all textures
 
-extern Texture2D test_object_0;
+extern Texture2D test_object_1;
 
 //all sounds
 
 extern Sound troll;
+
+//all currently spawned items
+extern item test_item;
+
+//extern vectors to keep track of the items
+extern std::vector <item*> spawned_items;
 
 //funcs
 void init_items();
 
 //ALL ITEM EXECUTION FUNCTIONS
 //format: *return type* *item name*_*id_func(*args*)
-//example: void test_obj_0_func()
+//example: void test_obj_1_func()
 
-void test_obj_0_func();
+void test_obj_1_func();
 
 #endif
