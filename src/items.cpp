f736b559a5ecf901c84f7258a5194d27d10bfb7f
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-10 22:26:48 +0300

    removed the item struct

--- src/items.cpp
+++ src/items.cpp
@@ -18,13 +18,18 @@ Texture2D test_object_1;
 //sound
 Sound troll;
 
+/*
+  NOTICE: for now i have no plans to use this struct system since i will just be using item ids to choose 
+  what to draw, what function it will do, etc
+
+*/
 
 // a vector to keep track of the spawned items
-std::vector <item*> spawned_items;
+//std::vector <item*> spawned_items;
 
 //item structs
 //check the general_parts header for more info
-item test_item;
+//item test_item;
 
 //load all item textures and init stuff
 void init_items() {
@@ -36,10 +41,10 @@ void init_items() {
     troll = LoadSound("resources/sfx/items/troll.ogg");
 
     //init all structs
-    test_item = {100, 200, 1, 2, "test obj", test_object_1};
+    //test_item = {100, 200, 1, 2, "test obj", test_object_1};
 
     //insert into vectors
-    spawned_items.push_back(&test_item);
+    //spawned_items.push_back(&test_item);
 }
 
 //ALL ITEM EXECUTION FUNCTIONS
