1cc1598a75d23ac54a8ac0e5f7ad63a171d7a4ae
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-10 22:23:16 +0300

    bruh

--- src/supermarket.cpp
+++ src/supermarket.cpp
@@ -4,13 +4,17 @@ extern "C" {
 }
 
 #include <vector>
-
 #include "headers/general_parts.hpp"
 
+//the background
 Texture2D floortileshop;
 
+//will have more parts here bro trust me
+
+//the initialize func, it loads texture data into memory when window is init
 void initshop()
 {
+    //background
 	floortileshop = LoadTexture("resources/img/textures/storebg1000x800.png");
 
 }
