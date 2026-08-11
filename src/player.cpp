b795e6d220df5c0ada6d5fccb9a66dde21496bc7
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-11 14:44:19 +0300

    update inventory to start with 3 items

--- src/player.cpp
+++ src/player.cpp
@@ -33,7 +33,7 @@ int playerspeed = 5;
 Rectangle guyHitbox;
 
 //the vector that contains all item ids in inventory
-std::vector <size_t> inventory_items = {1, 0, 0};
+std::vector <size_t> inventory_items = {1, 1, 1};
 
 //init variables and other stuff
 void init_player() {
