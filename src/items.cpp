9f9f9c3b408e96513c5bf0632c6e980b1aff9350
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-11 11:28:38 +0300

    implemented inventory system (read desc)
    !!!: it only works for the first slot for now as a test, but will work on all 3
    1. function to run every frame to check the inventory_items vector in player to see what item ids are in there, and if it finds a match (0 is void/nothing in that slot) then it draws the texture for it and detects when it is clicked, if so, execute the function to the item
    
    THANK YOU @FinnyFinn777  FOR THE IDEA THANK YOU!!!!!1

--- src/items.cpp
+++ src/items.cpp
@@ -4,6 +4,13 @@ extern "C" {
 
 #include "headers/general_parts.hpp"
 #include <vector>
+#include <stdio.h>
+
+//include player to manipulate values from it
+#include "headers/player.hpp"
+
+//extern the mouse hitbox since we will need it
+extern Rectangle mouseHitbox;
 
 //array that contains all possible item ids
 //check misc/all_item_ids.txt for all ids and respective object
@@ -57,4 +64,37 @@ void test_obj_1_func() {
     PlaySound(troll);
 }
 
+//the function for itemsd that it needs to run every loop
+void every_frame_func_items() {
 
+    //i know this will feel like "wtf man theres too many magic numbers" but i got an excuse
+    //im using an *ID* based item system
+    //im now immune haahahahahahah
+    
+    //jokes aside, yeah im checking in every slot to see what to render 
+    //in what item spot and what action to do when clicked
+    
+    switch(inventory_items.at(0)) {
+
+        //0 belongs to void, or nothing
+        case 0:
+
+        break;
+
+        //item 1 belongs to the test item
+        case 1:
+
+            DrawTexture(test_object_1, guySlot1X, guySlotY, WHITE);
+
+            //check if the user clicked on the icon using collisions
+            if (CheckCollisionRecs((Rectangle){guySlot1X, guySlotY, 32, 32}, mouseHitbox) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                //run the items's func
+                test_obj_1_func();
+
+                //then delete from inventory
+                inventory_items.at(0) = 0;
+            }
+
+        break;
+    }
+}
