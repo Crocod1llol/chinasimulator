b4411e6b5033c43b24f11c85ac08ec8b94df02d3
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-15 09:44:05 +0300

    added a door to exit
    you are not stuck :D

--- src/supermarket.cpp
+++ src/supermarket.cpp
@@ -4,17 +4,25 @@ extern "C" {
 }
 
 #include <vector>
+
 #include "headers/general_parts.hpp"
 
 //the background
 Texture2D floortileshop;
 
-//will have more parts here bro trust me
+//any created strutcs
+interact_part market_exit;
+
 
 //the initialize func, it loads texture data into memory when window is init
 void initshop()
 {
     //background
 	floortileshop = LoadTexture("resources/img/textures/storebg1000x800.png");
+
+    //any created structs
+    
+    //subtract y by height so it isnt outside the map
+    market_exit = {100, 800 - 150, 90, 150, LoadTexture("resources/img/textures/market_door_90x150.png") };
 
 }
