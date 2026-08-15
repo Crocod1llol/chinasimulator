9246245ea0dcf45d49567110bc0e62540170a2c7
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-15 09:44:53 +0300

    tell main that door exists
    no it doesnt. its just a shit project reason oj ok

--- src/headers/supermarket.hpp
+++ src/headers/supermarket.hpp
@@ -1,9 +1,15 @@
 #ifndef SUPERMARKET_HPP
 #define SUPERMARKET_HPP
 
+//tell header that structs are declared somewhere else
+struct interact_part;
+
+
 //the background
 extern Texture2D floortileshop;
 
+//any created structs
+extern interact_part market_exit;
 
 //funcs
 void initshop();
