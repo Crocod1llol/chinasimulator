fb860dbf73d034c2f1f6ad6b94f77158028196d2
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-22 17:38:05 +0300

    replace texture and remove part

--- src/home.cpp
+++ src/home.cpp
@@ -6,7 +6,6 @@ extern "C" {
 
 #include "headers/general_parts.hpp"
 
-
 //vectors containing all Parts
 //store pointers pf the interact parts so we can actually so the copies in the vector affect the real obj
 std::vector<interact_part*> home_int_parts;
@@ -21,18 +20,14 @@ interact_part outside_door_from_home;
 //separate interact items
 interact_part tester1;
 
-interact_part tester2;
-
 void init_home() {
 
     //initiate objects when the window is open
     outside_door_from_home = {350, 0, 90, 150, LoadTexture("resources/img/textures/door_90x150.png")};
-    tester1 = {20, 300, 100, 75, LoadTexture("resources/img/textures/test_100x75.png")};
-    tester2 = {600, 220, 100, 75, LoadTexture("resources/img/textures/test_100x75.png")};
+    tester1 = {20, 300, 100, 75, LoadTexture("resources/img/textures/chest_100x75.png")};
 
     //push to vectors
     home_int_parts.push_back(&tester1);
-    home_int_parts.push_back(&tester2);
 
     home_doors.push_back(outside_door_from_home);
 }
