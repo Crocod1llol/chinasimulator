ca46ec12dcfd3e27148140bc90cff98b5118d7ea
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-23 21:52:30 +0300

    made tester1 a container struct
    Refactor item initialization and management functions.

--- src/items.cpp
+++ src/items.cpp
@@ -2,115 +2,34 @@ extern "C" {
     #include "../lib/raylib.h"
 }
 
-#include "headers/general_parts.hpp"
 #include <vector>
-#include <stdio.h>
 
-//include player to manipulate values from it
-#include "headers/player.hpp"
+#include "headers/general_parts.hpp"
 
-//extern the mouse hitbox since we will need it
-extern Rectangle mouseHitbox;
+//vectors containing all Parts
+//store pointers pf the interact parts so we can actually so the copies in the vector affect the real obj
+std::vector<interact_part*> home_int_parts;
 
-//array that contains all possible item ids
-//check misc/all_item_ids.txt for all ids and respective object
-const unsigned int all_ids[] = {0, 1};
+std::vector <container*> home_containers;
 
+std::vector<Part> home_parts;
 
-//other resources
-//textures for all items
+std::vector<interact_part> home_doors;
 
-Texture2D test_object_1;
+//doors will be their own object since they go to different rooms
+interact_part outside_door_from_home;
 
-//sound
-Sound troll;
+//separate interact items
+container tester1;
 
-//load all item textures and init stuff
-void init_items() {
-    //init textures
-    test_object_1 = LoadTexture("resources/img/textures/items/1_test_obj.png");
+void init_home() {
 
+    //initiate objects when the window is open
+    outside_door_from_home = {350, 0, 90, 150, LoadTexture("resources/img/textures/door_90x150.png")};
+    tester1 = (container){20, 300, 100, 75, 6, {0, 1, 0, 0, 0, 0}, LoadTexture("resources/img/textures/chest_100x75.png")};
 
-    //init sounds
-    troll = LoadSound("resources/sfx/items/troll.ogg");
+    //push to vectors
+    home_containers.push_back(&tester1);
 
-    //init all structs
-    //test_item = {100, 200, 1, 2, "test obj", test_object_1};
-
-    //insert into vectors
-    //spawned_items.push_back(&test_item);
-}
-
-//ALL ITEM EXECUTION FUNCTIONS
-//format: *return type* *item name*_*id_func(*args*)
-//example: void test_obj_1_func()
-
-void test_obj_1_func() {
-    
-    //play a sound
-    PlaySound(troll);
-}
-
-//the function for itemsd that it needs to run every loop
-void every_frame_func_items() {
-
-    //i know this will feel like "wtf man theres too many magic numbers" but i got an excuse
-    //im using an *ID* based item system
-    //im now immune haahahahahahah
-    
-    //jokes aside, yeah im checking in every slot to see what to render 
-    //in what item spot and what action to do when clicked
-    
-    //vars to help me in what slot should i put the item based on what slot it is iterating rn
-    //the y is the same for all of them
-    int slotX = 0;
-
-    //iteratee over all slots
-    for (int i = 0; i < 3; i++) {
-
-        //select slot pos based on what slot the loop is in
-        switch (i) {
-            //these macro vars are made in player.hpp
-            case 0:
-                slotX = guySlot1X;
-
-            break;
-
-            case 1:
-                slotX = guySlot2X;
-
-            break;
-
-            case 2:
-                slotX = guySlot3X;
-
-            break;
-        }
-
-        switch(inventory_items.at(i)) {
-
-            //0 belongs to void, or nothing
-            case 0:
-
-            break;
-
-            //item 1 belongs to the test item
-            case 1:
-
-                DrawTexture(test_object_1, slotX, guySlotY, WHITE);
-
-                //check if the user clicked on the icon using collisions
-                //creating a rectangle on the spot to i dont have to make a struct
-                if (CheckCollisionRecs((Rectangle){(float)slotX, guySlotY, 32, 32}, mouseHitbox) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                    //run the items's func
-                    test_obj_1_func();
-
-                    //then delete from inventory
-                    inventory_items.at(i) = 0;
-                }
-
-            break;
-        }
-    }
-    
+    home_doors.push_back(outside_door_from_home);
 }
