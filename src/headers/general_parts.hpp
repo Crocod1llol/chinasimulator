ed9229da47aa889236c320e43aa2fe6c3aabdb66
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-23 09:25:05 +0300

    cleaning up comments

--- src/headers/general_parts.hpp
+++ src/headers/general_parts.hpp
@@ -50,50 +50,13 @@ typedef struct container {
 
     bool isInteracted = false;
 
-    const size_t max_items;
+    const int max_items;
 
-    std::vector <size_t> container_item_ids;
+    std::vector <int> container_item_ids;
 
     //the hitbox is larger so the player can interact with it without colliding directly into it
     Rectangle hitbox = {x, y, sizeX + 30, sizeY + 30};
 
 } container;
-
-//a struct to help me manage items
-/*
-  NOTICE: for now i have no plans to use this struct since i will just be using item ids to choose 
-  what to draw, what function it will do, etc
-
-*/
-
-/*
-typedef struct item {
-
-    float x;
-    float y;
-
-    //the item's id
-    //check misc for more info
-    size_t id;
-
-    //the status, whether its in a container or on the floor
-    //0 - in inventory
-    //1 - in container
-    //2 - on the floor
-    int status;
-
-    const char *name;
-
-    Texture2D tex;
-
-    //helpful for checking interactions
-    //default is false (0)
-    bool isInteracted = false;
-
-    //its hitbox
-    Rectangle hitbox = {x, y, 32, 32};
-
-} item;
-*/
 
 #endif
