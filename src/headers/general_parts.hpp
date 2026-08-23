75cd46a6ec19aa302a2891ab4bd27a4002c603a4
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-23 10:52:59 +0300

    changed the order in container

--- src/headers/general_parts.hpp
+++ src/headers/general_parts.hpp
@@ -46,13 +46,13 @@ typedef struct container {
     float sizeX;
     float sizeY;
 
-    Texture tex;
-
     bool isInteracted = false;
 
     const int max_items;
 
     std::vector <int> container_item_ids;
+
+    Texture tex;
 
     //the hitbox is larger so the player can interact with it without colliding directly into it
     Rectangle hitbox = {x, y, sizeX + 30, sizeY + 30};
