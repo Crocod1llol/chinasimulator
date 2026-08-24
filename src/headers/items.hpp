3f8ccbfe86ae25a2e2c389eebbe8083dc0984088
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-24 22:11:24 +0300

    open to main the function that transfers items to containers

--- src/headers/items.hpp
+++ src/headers/items.hpp
@@ -3,6 +3,7 @@
 
 //extern structs
 struct item;
+struct container;
 
 //array that contains all possible item ids
 //check misc/all_item_ids.txt for all ids and respective object
@@ -18,10 +19,18 @@ extern Texture2D test_object_1;
 
 extern Sound troll;
 
+//any eventual variables:
+
+//variable that is responsible for transmitting the inv interacted slot
+//default value is 255
+extern int inv_slot_interacted;
+
 
 //funcs
 void init_items();
 void every_frame_inv_func_items();
+
+void transfer_inv_to_cont(container *cont);
 
 void item_definer_6_slots(container *cont, Vector2 slot1, Vector2 slot2, Vector2 slot3, Vector2 slot4, Vector2 slot5, Vector2 slot6);
 
