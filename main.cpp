fe08c3f567d027b0c88ea84ef5efb16ace092406
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-24 22:13:04 +0300

    glued the functionallity to transfer inventory items to container

--- main.cpp
+++ main.cpp
@@ -217,11 +217,23 @@ int main(void) {
                 //the "GUI" if you interacted with the cool object
                 if (tester1.isInteracted) {
 
+                    //make the interact state to 1 so that the item transfer works
+                    inv_interact_state = 1;
+
+                    //oh and also run func that makes the item transfer possible when this is open
+                    transfer_inv_to_cont(&tester1);
 
                     DrawTexture(container_6_slots, screenWidth/2 - (300 / 2), screenHeight/2 - (300 / 2), WHITE);
 
                     //draw all items in the container
                     item_definer_6_slots(&tester1, (Vector2){393, 289}, (Vector2){487, 289}, (Vector2){569, 289}, (Vector2){396, 381}, (Vector2){487, 381}, (Vector2){567, 381});
+                } else {
+                    //honestly its just so item transfer is not sticky
+                    inv_interact_state = 0;
+
+                    //and also to reset the variable that decides which slot to switch
+                    //its default value is 255
+                    inv_slot_interacted = 255;
                 }
 
                 break;
