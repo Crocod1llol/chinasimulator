82f1ce88c3e684ffcd4df434a80624ac1fb41ed7
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-08-28 20:54:42 +0300

    instead of including every file, use a friggin wildcard for all .cpp file

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/player.cpp ./src/home.cpp ./src/outside_home.cpp ./src/items.cpp ./src/supermarket.cpp -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/*.cpp ./src/supermarket.cpp -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
