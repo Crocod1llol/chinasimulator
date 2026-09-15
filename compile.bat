ee04984d49dd0b282872ecd3c39abf20833cb2dd
Crocod1llol <138702181+Crocod1llol@users.noreply.github.com>
2026-09-15 07:15:14 +0300

    the filepath searched mainly for lib_win64, but you can modify it to lib_win32

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/*.cpp ./src/supermarket.cpp -L./lib_win/ -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ -Werror -Wunused-macros -Wall -Wextra -o game main.cpp ./src/*.cpp ./src/supermarket.cpp -L./lib_win64/ -lraylib -lopengl32 -lgdi32 -lwinmm
